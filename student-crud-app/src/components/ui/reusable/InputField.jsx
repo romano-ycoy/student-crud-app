3c3bd5f6293c294d0c2942c3f9e4923d9ec1f533
romano-ycoy <romanoycoy1@gmail.com>
2025-09-26 13:01:10 +0800

    commit
    Added a spacing between the log in form elements & added font poppins to the input text.

--- student-crud-app/src/components/ui/reusable/InputField.jsx
+++ student-crud-app/src/components/ui/reusable/InputField.jsx
@@ -38,7 +38,8 @@ const InputField = ({
 
             <input
                 id={id}
-                className={`border border-gray-300 rounded-sm focus:ring-2 focus:ring focus:ring-red-500 focus:outline-none h-10 px-3 mb-4 placeholder-gray-400 placeholder:font-poppins${className}`}
+                className={`border border-gray-300 rounded-sm focus:ring-2 focus:ring focus:ring-red-500 focus:outline-none h-10 px-3 mb-4 placeholder-gray-400
+                font-poppins placeholder:font-poppins ${className}`}
                 type={inputType}
                 value={value}
                 onChange={onChange}
